9c1aa1efe9bd93ab2569d31f74f51e4132de47d1
Daur <daur2001@mail.ru>
2023-01-31 13:21:38 +0300

    onClick, ref, VirtualDOM

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,6 +8,12 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
     let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
 
+    let newMessageElement = React.createRef();
+    let sendMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text)
+    }
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
@@ -15,7 +21,14 @@ const Dialogs = (props) => {
             </div>
             <div className={classes.messages}>
                 {messagesElements}
-
+                <div className={classes.addPost}>
+                    <div>
+                        <textarea ref={newMessageElement}></textarea>
+                    </div>
+                    <div>
+                        <button onClick={ sendMessage }>Send a message</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
