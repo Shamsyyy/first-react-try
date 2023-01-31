df3b0779d29e472afae1eddb8a26cc53ede0efea
Daur <daur2001@mail.ru>
2023-02-01 01:08:35 +0300

    new render.js a bit of flux-concept

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,4 @@
+import {rerenderEntireTree} from "../render";
 
 
 let state = {
@@ -45,6 +46,7 @@ export let addPost = (postMessage) => {
         likesCount: 0
     }
     state.profilePage.posts.push(newPost)
+    rerenderEntireTree(state);
 }
 
 export default state;
