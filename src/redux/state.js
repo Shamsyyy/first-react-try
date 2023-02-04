104a9c78948e72905f712f4bc442f32095356e67
Daur <daur2001@mail.ru>
2023-02-04 15:23:36 +0300

    action-creator, action-type

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,9 @@
+const ADD_POST = 'ADD-POST';
+const UNDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const ADD_MESSAGE = 'ADD-MESSAGE';
+const UNDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
+
+
 let store = {
     _state: {
         profilePage: {
@@ -80,7 +86,7 @@ let store = {
     },*/
 
     dispatch(action) { // { type: 'ADD-POST' }
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
 
             let newPost = {
                 id: 5,
@@ -91,12 +97,12 @@ let store = {
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
 
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UNDATE_NEW_POST_TEXT) {
 
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
 
-        }else if (action.type === 'ADD-MESSAGE') {
+        }else if (action.type === ADD_MESSAGE) {
 
             let NewMessage = {
                 id: 6,
@@ -106,7 +112,7 @@ let store = {
             this._state.dialogsPage.newMessageText = '';
             this._callSubscriber(this._state);
 
-        } else if (action.type === 'UPDATE-NEW-MESSAGE-TEXT') {
+        } else if (action.type === UNDATE_NEW_MESSAGE_TEXT) {
 
             this._state.dialogsPage.newMessageText = action.newText;
             this._callSubscriber(this._state);
@@ -115,6 +121,14 @@ let store = {
     }
 }
 
+export const addPostActionCreator = () => ({ type: ADD_POST })
+export const updateNewPostTextActionCreator = (text) => ({
+    type: UNDATE_NEW_POST_TEXT, newText: text
+})
+export const addMessageActionCreator = () => ({ type: ADD_MESSAGE })
+export const updateNewMessageTextActionCreator = (text) => ({
+        type: UNDATE_NEW_MESSAGE_TEXT, newText: text
+})
 
 window.store = store;
 
