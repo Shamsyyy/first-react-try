0901bbf0fae40a555593cf0b15e04e778ec2270b
Daur <daur2001@mail.ru>
2023-02-12 15:25:16 +0300

    getting rid of side effects
    remade user.js into a "cleaner component"

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,18 +4,21 @@ import axios from "axios";
 import userPhoto from "../../assets/image/user.png"
 
 const Users = (props) => {
-
-    if (props.users.length === 0) {
-        axios
-            .get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                debugger;
-                props.setUsers(response.data.items)
-            });
+    let getUsers = () =>
+    {
+        if (props.users.length === 0) {
+            axios
+                .get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    debugger;
+                    props.setUsers(response.data.items)
+                });
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
