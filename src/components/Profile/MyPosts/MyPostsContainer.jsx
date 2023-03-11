8da60758974bf0769244c1841661b98f542dd2a9
Daur <daur2001@mail.ru>
2023-03-11 16:18:43 +0300

    refactoring

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -3,52 +3,17 @@ import {addPostCreator} from "../../../redux/profileReducer";
 import MyPosts from "./MyPosts";
 import {connect} from "react-redux";
 
-// const MyPostsContainer1 = (props) => {
-//     // let state = props.store.getState();
-//     //
-//     // let addPost = () => {
-//     //     props.store.dispatch(addPostCreator());
-//     // }
-//     //
-//     // let onPostChange = (text) => {
-//     //     props.store.dispatch(updateNewPostTextCreator(text));
-//     // }
-//
-//     return (
-//         <StoreContext.Consumer>
-//             {store => {
-//                 let state = store.getState();
-//
-//                 let addPost = () => {
-//                     store.dispatch(addPostCreator());
-//                 }
-//
-//                 let onPostChange = (text) => {
-//                     store.dispatch(updateNewPostTextCreator(text));
-//                 }
-//
-//                 return <MyPosts updateNewPostText={onPostChange}
-//                                 addPost={addPost}
-//                                 posts={state.profileReducer.posts}
-//                                 newPostText={state.profileReducer.newPostText}/>
-//             }
-//             }
-//         </StoreContext.Consumer>
-//     )
-// }
+
 
 let mapStateToProps = (state) => {
     return {
         posts: state.profileReducer.posts,
-        // newPostText: state.profileReducer.newPostText
+
     }
 
 }
 let mapDispatchToProps = (dispatch) => {
     return {
-/*        updateNewPostText: (text) => {
-            dispatch(updateNewPostTextCreator(text));
-        },*/
         addPost: (newPostText) => {
             dispatch(addPostCreator(newPostText));
         }
