1bf568045be4dc6a98720779cb2b25e8f14c830c
Shhhamsy <daur2001@mail.ru>
2023-05-19 18:37:51 +0300

    fixed paginator

--- src/components/Common/Paginator/Paginator.tsx
+++ src/components/Common/Paginator/Paginator.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import classes from "./Paginator.module.css"
 import cn from "classnames";
 
@@ -8,13 +8,15 @@ type PropsType = {
     onPageChanged: (pageNumber: number) => void,
     currentPage: number,
     portionSize?: number
+    setCurrentPage: any
 }
 
 let Paginator: React.FC<PropsType> = ({totalItemsCount,
-                                      pageSize ,
-                                      onPageChanged,
-                                      currentPage,
-                                      portionSize = 10}) => {
+                                          pageSize ,
+                                          onPageChanged,
+                                          currentPage,
+                                          setCurrentPage,
+                                          portionSize = 10}) => {
 
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
@@ -29,20 +31,17 @@ let Paginator: React.FC<PropsType> = ({totalItemsCount,
     let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
     let rightPortionPageNumber = portionNumber * portionSize;
 
-
-    // previous version
-/*  let curP = currentPage;                          //Карусель массива номеров страниц
-    let curPF = ((curP - 5) < 0) ? 0 : curP - 5;     //Begin
-    let curPL = curP + 4;                            //End
-    let slicedPages = pages.slice(curPF, curPL);     //сделано через Array.slice():
-    debugger*/
+    useEffect(() => {
+        let portionNumber = Math.ceil(currentPage / portionSize);
+        setPortionNumber(portionNumber);
+    }, [currentPage, portionSize]);
 
     return (
         <div className={classes.paginator}>
             { portionNumber > 1 &&
                 <button onClick={() => { setPortionNumber(portionNumber - 1) }}>PREV</button> }
             {pages
-                .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber)
+                .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber&& p<=rightPortionPageNumber)
                 .map((p) => {
                     return <span className={ cn({
                         [classes.selectedPage]: currentPage === p
@@ -50,6 +49,7 @@ let Paginator: React.FC<PropsType> = ({totalItemsCount,
                                  key={p}
                                  onClick={(e) => {
                                      onPageChanged(p);
+                                     setCurrentPage(p); // обновляем currentPage
                                  }}>{p}</span>
                 })}
             { portionCount > portionNumber &&
